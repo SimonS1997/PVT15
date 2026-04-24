f8051a9e2c6f428dfdcc11bc3248fb9161de8e1b
jonas <jonasbruhnerici@gmail.com>
2026-04-24 10:38:16 +0200

    Kotlin Uppdaterad till 2.1.10 Löste Gradlestrul - sätt IDE till java 25 (Build använder fortfarande java 21 - se build.)

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.24"
+    kotlin("jvm") version "2.1.10"
+    kotlin("plugin.spring") version "2.1.10"
     id("org.springframework.boot") version "3.3.2"
     id("io.spring.dependency-management") version "1.1.6"
 }
@@ -29,6 +29,7 @@ dependencies {
 }
 
 kotlin {
+    jvmToolchain(21)
     compilerOptions {
         freeCompilerArgs.addAll("-Xjsr305=strict")
     }
